fef3c227eea8290b3a4af1fa03d4fad7602cbc4b
james.longley@bcs.org <james.longley@bcs.org>
2018-12-05 12:53:10 +0000

    getting data back

--- models/server_db.js
+++ models/server_db.js
@@ -10,6 +10,15 @@ save_user_information = (data) => new Promise((resolve, reject)=>{
   });
 })
 
+get_total_amount = (data) => new Promise((resolve, reject)=>{
+  db.query('SELECT sum(amount) as total_amount from lottery_information', data, function (err,results, fields){
+
+    if(err){
+      reject('Could not get total amount');
+    }
+    resolve(results);
+  });
+})
 module.exports = {
   save_user_information
 }
